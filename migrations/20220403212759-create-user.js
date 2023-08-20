0467526edf0a8cc82042f4c71b35e12c30f6d850
boll84011336 <boll84011336@gmail.com>
2023-08-20 23:42:35 +0800

    變更usersInfo名稱

--- migrations/20220403212759-create-user.js
+++ migrations/20220403212759-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('UsersInfo', {
+    await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('UsersInfo');
+    await queryInterface.dropTable('Users');
   }
 };
